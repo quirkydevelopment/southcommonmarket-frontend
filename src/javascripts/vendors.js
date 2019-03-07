f5787787941d8dc895106847cc196ec771520305
wendave <owendavecaisip@gmail.com>
2019-03-07 22:40:27 +0800

    desktop - responsive - in progress

--- src/javascripts/vendors.js
+++ src/javascripts/vendors.js
@@ -1,4 +1,5 @@
 import '../../node_modules/jquery/dist/jquery'
 import '../../node_modules/popper.js/dist/popper'
+import '../../node_modules/slick-carousel/slick/slick'
 import '../../node_modules/tether/dist/js/tether'
 import '../../node_modules/bootstrap/dist/js/bootstrap'
